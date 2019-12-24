217ce1fbf49f297713dc038b300a67e008d45d93
taciomcosta <taciomcosta@gmail.com>
2019-12-24 15:48:51 -0200

    feat: add content to memoirs (expect for Empire Estate)

--- webapp/js/languages/en_en.js
+++ webapp/js/languages/en_en.js
@@ -36,60 +36,72 @@ export const dynamic_en_en = {
       "id": "easyVisionCase",
       "name": "EasyVision",
       "image": "easyvision.png",
-      "description": "Here goes your description about EasyVision",
+      "description": "EasyVision is a B2B Energy Efficiency Solution that helps on monitoring and maintenance processes of thousands of Building Management Systems spread over many countries.",
       "techStack": ["NodeJS", "Angular", "Typescript", "Azure"],
       "contributions": [
-        "ev 1",
-        "ev 2",
-        "ev 3",
+        "Implemented 6 Building Management System APIs using Python, NodeJS and PHP, which was part of a Big Data \
+        solution that reduced the time spent for monitoring IoT data from weeks to minutes;",
+        "Responsible for migrating from a monolithic service architecture to a modern microservice architecture. This \
+        allowed the company to support any new B2B/B2C solution at lower costs;",
+        "Guided 4 teams through software development best practices such as TDD, Clean Architecture, DDD and \
+        DevOps Tools (Kubernetes, Docker, Azure Pipelines and Git Flow), helping the company to have a maintainable source code \
+        and a sharing knowledge culture;",
       ]
     },
     {
       "id": "codyCrossCase",
       "name": "Cody Cross",
       "image": "codycross.png",
-      "description": "Here goes your description about Cody Cross",
-      "techStack": [".NET Core", "Unitiy", "Python", "AWS"],
+      "description": "CodyCross is a word game unlike any other. Follow this alien that has crash-landed on Earth. He counts on your help to explore the world and discover its mysteries by solving puzzles",
+      "techStack": [".NET Core", "Unity", "Python", "AWS"],
       "contributions": [
-        "cc 1",
-        "cc 2",
-        "cc 3",
+        "Automated user ratings monitoring processes using Python, which helped the company to rapidly detect \
+        abnormalities in their mobile games. Used Google Play Store and App Store API's;",
+        "Planned, configured and implemented several unit and integration tests using tools such as Fabric.io, which \
+        assured 95% of bug and crash-free sessions in all the games with millions of users and daily accesses;",
       ]
     },
     {
       "id": "moodleCase",
       "name": "Moodle",
       "image": "moodle.png",
-      "description": "Here goes your description about Moodle",
+      "description": "Moodle is a free and open-source learning management system written in PHP and distributed under the GNU General Public License.",
       "techStack": ["PHP", "JavaScript", "CSS", "HTML"],
       "contributions": [
-        "mm 1",
-        "mm 2",
-        "mm 3",
+        "Developed custom look & feel for UniSantaRita using web technologies such as HTML5 and CSS3. This way \
+        the students had a better user experience in the platform and the customers demand decreased considerably;",
+        "Managed user data stored in a SQL-based database, helping the institution to keep a high integrity on their students data \
+        for a long term;",
+        "Helped in managing on-premise servers running Moodle's system and other internal tools essential for the institution, so that \
+        they could have a healthy computer network environment;",
       ]
     },
     {
       "id": "hydroCodeCase",
       "name": "Hydro Code",
       "image": "hydrocode.png",
-      "description": "Here goes your description about HydroCode",
+      "description": "HydroCode is a scalable water consumption management system and one of the SAP Innomarathon 2017 \
+      semifinalist startups.",
       "techStack": ["NodeJS", "React", "Python", "Raspberry Pi"],
       "contributions": [
-        "hc 1",
-        "hc 2",
-        "hc 3",
+        "Designed the core architecture using a Microservices approach, allowing a software construction decoupled from \
+        all the IoT devices. Thus, those devices could easily be replaced without too much effort and costs;",
+        "Implemented a web application written in ReactJS that was used as a prototype interface for a Industry 4.0 \
+        integrated environment;",
+        "Implemented notifications and real-time consumption services using NodeJS and Websockets, that allowed \
+        the system to process received consumption data and notify the users right away in case of any abnormality;",
       ]
     },
     {
       "id": "chessEngineCase",
       "name": "Chess Engine",
       "image": "chessengine.jpg",
-      "description": "Here goes your description about Chess Engine",
+      "description": "Personal project used as a playground for developing new skills.",
       "techStack": ["Java", "OpenGL", "LWGL"],
       "contributions": [
-        "ce 1",
-        "ce 2",
-        "ce 3",
+        "Designed and implemented its core features such as multiplayer and chess gameplay using Java and TDD;",
+        "Implemented a first version of an AI using Minimax and Alpha-Beta pruning algorithms;",
+        "Started the graphical module implementation using OpenGL Rendering Pipelines and Lighweight Java Game Library;",
       ]
     },
     {
--- webapp/js/languages/pt_br.js
+++ webapp/js/languages/pt_br.js
@@ -37,60 +37,69 @@ export const dynamic_pt_br = {
       "id": "easyVisionCase",
       "name": "EasyVision",
       "image": "easyvision.png",
-      "description": "Descrição aqui",
+      "description": "EasyVision é uma solução de Eficiência Energética B2B que auxilia nos processos de monitoramento e manutenção de milhares de Sistema Automação Predial \
+      espalhados por diversos países.",
       "techStack": ["NodeJS", "Angular", "Typescript", "Azure"],
       "contributions": [
-        "ev 1",
-        "ev 2",
-        "ev 3",
+        "Implementou 6 APIs de Sistemas de Automação Predial, utilizando Python, NodeJS e PHP, o que foi parte de uma solução de Big Data que reduziou o tempo gasto \
+        no monitormaneto de dados de IoT de semanas para minutos;",
+        "Responsável por migrar de uma arquitetura de serviços monolítica para uma arquitetura moderna de microsserviços. Isso \
+        possibilitou que a empresa desenvolvesse qualquer nova solucação B2B/B2C com menores custos;",
+        "Guiou 4 times através de boas práticas de desenvolvimento de software tais como TDD, Clean Architecture, DDD e ferramentas de DevOps \
+        (Kubernetes, Docker, Azure Pipelines e Git Flow), ajudando a empresa a ter uma base de código manutenível e uma cultura de compartilhamento de conhecimento;",
       ]
     },
     {
       "id": "codyCrossCase",
       "name": "Cody Cross",
       "image": "codycross.png",
-      "description": "Descrição aqui",
-      "techStack": [".NET Core", "Unitiy", "Python", "AWS"],
+      "description": "CodyCross é um jogo de palavras inédito. Acompanhe esse alienígena que veio parar na Terra devido a um acidente com sua nave. Ele conta com a sua ajuda para explorar o mundo e descobrir seus mistérios através de palavras cruzadas.",
+      "techStack": [".NET Core", "Unity", "Python", "AWS"],
       "contributions": [
-        "cc 1",
-        "cc 2",
-        "cc 3",
+        "Automatizou processos de monitoramento de avaliação dos usuários usando Python, o que ajudou a empresa a rapidamente detectar comportamentos anormais em seus jogos mobile. \
+        Foram utilizados as APIs da Google Play Store e App Store para isso;",
+        "Planejou, configurou e implementou testes de integração e unidade utilizando ferramentas como Fabric.io, que garantiu 95% de sessões sem bugs e crashes \
+        em todos os jogos com milhões de usuários e acessos diários;",
       ]
     },
     {
       "id": "moodleCase",
       "name": "Moodle",
       "image": "moodle.png",
-      "description": "Descrição aqui",
+      "description": "Moodle é um sistema de gestão de aprendizagem gratuito e código aberto, escrito em PHP e distribuido sob a licença GNU Public License.",
       "techStack": ["PHP", "JavaScript", "CSS", "HTML"],
       "contributions": [
-        "mm 1",
-        "mm 2",
-        "mm 3",
+        "Desenvolveu um look & feel customizado para a UniSantaRita, utilizzando tecnologies como HTML5 e CSS3. Assim, os estudantes contaram com uma user experience melhorada\
+        na plataforma e a demanda por suporte diminui consideravelmente; ",
+        "Gerenciou dados de usuário armazenados em um banco baseado em SQL, ajudando a instituição a manter uma alta integridade sobre os dados dos estudantes por um longo perído;",
+        "Ajudou a gerenciar servidores on-premise que suportavam a plataforma Moodle e outras ferramentas internas essencias para a instituição, contribuindo para um \
+        ambiente de redes de computadores saudável;"
       ]
     },
     {
       "id": "hydroCodeCase",
       "name": "Hydro Code",
       "image": "hydrocode.png",
-      "description": "Descrição aqui",
+      "description": "Hydro Code é um sistema de gerenciamento de consumo de água escalável e uma das startups semifinalistas da SAP Innomarathon 2017.",
       "techStack": ["NodeJS", "React", "Python", "Raspberry Pi"],
       "contributions": [
-        "hc 1",
-        "hc 2",
-        "hc 3",
+        "Projetou a arquitetura core utilizando uma abordagem de microsserviços e permitindo uma construção de software desacoplada de todos dispositivos de IoT.\
+        Assim, esses dispositivos poderiam ser facilmente substituidos sem muito esforço e custo;",
+        "Implementou uma aplicação web escrita com ReactJS, que foi utilizada como uma interface de protótipo para um ambiente integrado de Indústria 4.0;",
+        "Implementou os serviços de notificações e de consumo de água em tempo real utilizando NodeJS e Websockets, permitindo ao sistema processar dados \
+        de consumo recebidos e notificar os usuários imediamente em caso de anormalidades;",
       ]
     },
     {
       "id": "chessEngineCase",
       "name": "Chess Engine",
       "image": "chessengine.jpg",
-      "description": "Descrição aqui",
+      "description": "Projeto pessoal utilizado como 'plaground' para desenvolver novas habilidades.",
       "techStack": ["Java", "OpenGL", "LWGL"],
       "contributions": [
-        "ce 1",
-        "ce 2",
-        "ce 3",
+        "Projetou e implementou as funcionalidades principais, tais como multiplayer e a gameplay de xadrez, utilizando Java e TDD;",
+        "Implementou a primeira versão de uma IA, utilizando os algoritmos Minimax e Alpha-Beta Pruning;",
+        "Começou a implementação do módulo gráfico, utilizando OpenGL Rendering Pipelines e a biblioteca Lightweight Java Game;",
       ]
     },
     {
